ac992438bafa16749bc60106a79cbbd4234438fd
Crowdbotics <engineering@crowdbotics.com>
2023-04-25 12:11:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-frost-dev-78958",
+  url: "https://sweet-frost-dev-78958.botics.co",
+  api: "https://sweet-frost-dev-78958.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
